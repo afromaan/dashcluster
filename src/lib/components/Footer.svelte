936fdb7bf059ff52e946ad898b2e301c76ad66e6
Dominik Bartoněk <dominik.bartonek@seznam.cz>
2026-01-23 16:21:48 +0100

    add frontend

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,63 @@
+<footer>
+    <div class="container text-center py-4">
+        <div class="row">
+            <div class="col-4">
+                <nav>
+                    <ul class="navbar-nav p-3 d-flex justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Services</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-4">
+                <nav>
+                    <ul class="navbar-nav p-3 d-flex justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Privacy Policy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Terms of Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-4">
+                <nav>
+                    <ul class="navbar-nav p-3 d-flex justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Facebook</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Twitter</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Instagram</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <p class="mb-0">&copy; 2026 ProtoByte. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer {
+        background-color: #222;
+        color: #fff;
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        box-shadow: 0 -2px 5px rgba(255, 255, 255, 0.1);
+    }
+</style>
