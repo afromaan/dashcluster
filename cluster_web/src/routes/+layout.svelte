2ffbb3cabd40fb15431d324d5e8d5a3db6cc661c
Dominik Bartoněk <dominik.bartonek@seznam.cz>
2026-01-23 20:59:13 +0100

    uprava pro jebnutyho ulicha

--- cluster_web/src/routes/+layout.svelte
+++ cluster_web/src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import Header from "$lib/components/Header.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import "../app.css";
-	import "mdb-ui-kit/css/mdb.min.css";
 </script>
 
 <Header />
